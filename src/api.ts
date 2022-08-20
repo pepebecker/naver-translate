eab853076e35c00662a569f5707bed57db0fd450
Pepe Becker <mail@pepebecker.com>
2022-08-20 22:13:35 +0200

    Update API
    Signed-off-by: Pepe Becker <mail@pepebecker.com>

--- src/api.ts
+++ src/api.ts
@@ -12,7 +12,9 @@ export const fetchEntry = async (
   if (typeof srcDest === 'boolean') srcDest = srcDest ? 'enko' : 'koen';
   const baseUrl = `https://en.dict.naver.com/api/platform/${srcDest}`;
   const url = `${baseUrl}/entry?entryId=${entryId}`;
-  const response = await axios.get(url);
+  const response = await axios.get(url, { headers: {
+    referer: 'https://en.dict.naver.com/',
+  }});
   return response.data;
 };
 
@@ -21,6 +23,8 @@ export const search = async (query: string, srcDest: SrcDest = 'koen') => {
   const encoded = encodeURIComponent(query);
   const baseUrl = `https://en.dict.naver.com/api3/${srcDest}`;
   const url = `${baseUrl}/search?query=${encoded}`;
-  const response = await axios.get(url);
+  const response = await axios.get(url, { headers: {
+    referer: 'https://en.dict.naver.com/',
+  }});
   return response.data;
 };
